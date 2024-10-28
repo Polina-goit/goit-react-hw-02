012fd7ab6fa1d69b6b4846c04e08043331a99040
Polina <polinakoctyu4enko@gmail.com>
2024-10-28 21:15:55 +0200

    step-3

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Description from "./components/Description/Description";
 import Feedback from "./components/Feedback/Feedback";
 import Options from "./components/Options/Options";
+import Notification from "./components/Notification/Notification";
 import "./App.css";
 
 function App() {
@@ -19,13 +20,16 @@ function App() {
       [feedbackType]: feedback[feedbackType] + 1,
     });
   };
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
   return (
     <>
       <Description />
       <Options
         onClickFeedback={(feedbackType) => updateFeedback(feedbackType)}
       />
-      <Feedback counter={feedback} />
+      {totalFeedback >= 1 && <Feedback counter={feedback} />}
+      {totalFeedback < 1 && <Notification />}
     </>
   );
 }
